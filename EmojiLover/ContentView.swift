8472f6d8992ff2baa53e24c0c20ae7aa5a82f163
Oganbelema <oganbelema@gmail.com>
2024-04-23 16:23:09 +0100

    Add navigation view

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 👍🏾, 😅, 🙌🏾, 🎉, 🌴
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .🎉
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self){
+                        Text($0.rawValue)
+                    }
+                }.pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
         }
-        .padding()
     }
 }
 
